fea9711c35741a75f72f655dc9c181915aaa23da
AlexBaily <alex.d.baily@gmail.com>
2022-01-23 15:26:49 +0000

    Add connection handler.

--- sockets/sockets.go
+++ sockets/sockets.go
@@ -9,16 +9,19 @@ func StartServer(connPort string) {
 
 	ln, _ := net.Listen("tcp", connPort)
 
-	conn, err := ln.Accept()
-	if err != nil {
-		fmt.Println(err)
-	}
 
 	for {
-		fmt.Println(conn)
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+	
+		go connHandler(conn)
 	}
 }
 
-//func connHandler(conn) {
+func connHandler(conn net.Conn) {
+	defer conn.Close()
 
-//}
+	conn.Write([]byte("Connection successful."))
+}
